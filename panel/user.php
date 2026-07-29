03668e9898e7dfafcc065795164341651f370dd0
CubePay <122695122+cubepy@users.noreply.github.com>
2026-07-29 23:53:49 +0330

    Add files via upload

--- panel/user.php
+++ panel/user.php
@@ -352,6 +352,7 @@ include __DIR__ . '/inc/layout_head.php';
                                     'low balance by admin' => $textbotlang['panel']['userMethodAdminDeduct'],
                                     'zarinpal' => $textbotlang['panel']['userMethodZarinpal'],
                                     'aqayepardakht' => $textbotlang['panel']['userMethodAqayePardakht'],
+                                    'cubepay' => $textbotlang['panel']['userMethodCubepay'],
                                     'plisio' => 'Plisio',
                                     'nowpayment' => 'NowPayment',
                                     'Star Telegram' => $textbotlang['panel']['userMethodTelegramStar'],
